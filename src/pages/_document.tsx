d9c7355f18f1991a3136e65da4cf1a648e21f789
httpjamesm <github@httpjames.space>
2024-06-11 01:27:59 -0400

    feat: favicon and meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>AO Instances</title>
+        <link rel="icon" href="/favicon.webp" />
+        <meta
+          name="description"
+          content="Find an AnonymousOverflow instance to get programming help without compromising your privacy."
+        />
+        <meta name="theme-color" content="#8CFEC0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
